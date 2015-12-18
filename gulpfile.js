dd4809f51912098a325022ea887fbea23896b6dd
Robin Linus <robin_woll@capira.de>
2015-12-18 17:43:46 +0100

    Import files from Yawim

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,4 @@
-/*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
+
 
 'use strict';
 
@@ -236,7 +229,7 @@ gulp.task('clean', function() {
 });
 
 // Watch files for changes & reload
-gulp.task('serve', ['lint', 'styles', 'elements', 'images'], function() {
+gulp.task('serve', [ 'styles', 'elements', 'images'], function() {
   browserSync({
     port: 5000,
     notify: false,
