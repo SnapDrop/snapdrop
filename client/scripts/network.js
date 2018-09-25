61351cd0222282db9d33ac08de04788832f9ec1c
RobinLinus <robinlinus@users.noreply.github.com>
2018-09-25 18:29:35 +0200

    Fix depricated api

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -496,7 +496,7 @@ RTCPeer.config = {
     }, {
         urls: 'stun:stun.l.google.com:19302'
     }, {
-        url: 'turn:192.158.29.39:3478?transport=tcp',
+        urls: 'turn:192.158.29.39:3478?transport=tcp',
         credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
         username: '28224511:1379330808'
     }]
