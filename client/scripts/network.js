cf9129618bfb77543da80cd36daa04e21951310b
RobinLinus <RobinLinus@users.noreply.github.com>
2019-02-18 22:19:07 +0100

    Remove unnecessary code

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -274,11 +274,9 @@ class RTCPeer extends Peer {
         if (message.sdp) {
             this._conn.setRemoteDescription(new RTCSessionDescription(message.sdp))
                 .then( _ => {
-                    if (message.sdp.type == 'offer') {
+                    if (message.sdp.type === 'offer') {
                         return this._conn.createAnswer()
                             .then(d => this._onDescription(d));
-                    } else {
-                        return null;
                     }
                 })
                 .catch(e => this._onError(e));
