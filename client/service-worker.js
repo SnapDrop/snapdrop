2349d0b20b1799d006e4ee1cace492c56f671b10
RobinLinus <RobinLinus@users.noreply.github.com>
2020-05-19 22:52:27 +0200

    Update service-worker.js

--- client/service-worker.js
+++ client/service-worker.js
@@ -33,3 +33,22 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
+
+
+self.addEventListener('activate', function(event) {
+  console.log('Updating Service Worker...')
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+          return true
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
