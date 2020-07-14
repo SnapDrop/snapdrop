308aabac3243559706f20e4d129908da87c2ed1c
Paul Szymanski <519954+PaulSzymanski@users.noreply.github.com>
2020-07-14 22:55:52 +0200

    Remove offline TURN server

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -511,9 +511,5 @@ class Events {
 RTCPeer.config = {
     'iceServers': [{
         urls: 'stun:stun.l.google.com:19302'
-    }, {
-        urls: 'turn:192.158.29.39:3478?transport=tcp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
     }]
 }
