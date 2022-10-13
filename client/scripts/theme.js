7d4e67b8793762e9a581b3685ea7556fb5abe99d
Giorgio Bellisario <72039923+Bellisario@users.noreply.github.com>
2022-10-13 17:55:49 +0200

    JS, HTML and CSS fmt + NPM lockfile version bump
    Formatted all JS, CSS and HTML code.
    Also updated server lockfile version (package-lock.json) from 1 to 2.

--- client/scripts/theme.js
+++ client/scripts/theme.js
@@ -1,24 +1,24 @@
-(function(){
-  
+(function () {
+
   // Select the button
   const btnTheme = document.getElementById('theme');
   // Check for dark mode preference at the OS level
   const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-   
+
   // Get the user's theme preference from local storage, if it's available
   const currentTheme = localStorage.getItem('theme');
   // If the user's preference in localStorage is dark...
   if (currentTheme == 'dark') {
     // ...let's toggle the .dark-theme class on the body
     document.body.classList.toggle('dark-theme');
-  // Otherwise, if the user's preference in localStorage is light...
+    // Otherwise, if the user's preference in localStorage is light...
   } else if (currentTheme == 'light') {
     // ...let's toggle the .light-theme class on the body
     document.body.classList.toggle('light-theme');
   }
-   
+
   // Listen for a click on the button 
-  btnTheme.addEventListener('click', function() {
+  btnTheme.addEventListener('click', function () {
     // If the user's OS setting is dark and matches our .dark-theme class...
     if (prefersDarkScheme.matches) {
       // ...then toggle the light mode class
--- client/service-worker.js
+++ client/service-worker.js
@@ -11,11 +11,11 @@ var urlsToCache = [
   'images/favicon-96x96.png'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(function (cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
@@ -23,32 +23,32 @@ self.addEventListener('install', function(event) {
 });
 
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request);
       }
-    )
+      )
   );
 });
 
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   console.log('Updating Service Worker...')
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.filter(function(cacheName) {
+        cacheNames.filter(function (cacheName) {
           // Return true if you want to remove this cache,
           // but remember that caches are shared across
           // the whole origin
           return true
-        }).map(function(cacheName) {
+        }).map(function (cacheName) {
           return caches.delete(cacheName);
         })
       );
