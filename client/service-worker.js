5db51b16ad705cc77fa0284f21c3041a7a2fa1fc
yjmp14 <yjmp14@gmail.com>
2022-11-17 19:21:16 +0800

    Rewrite service-worker.js, make the cache function work correctly.

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,57 +1,43 @@
 var CACHE_NAME = 'snapdrop-cache-v2';
 var urlsToCache = [
-  'index.html',
-  './',
-  'styles.css',
-  'scripts/network.js',
-  'scripts/ui.js',
-  'scripts/clipboard.js',
-  'scripts/theme.js',
-  'sounds/blop.mp3',
-  'images/favicon-96x96.png'
+    './',
+    'index.html',
+    'styles.css',
+    'scripts/network.js',
+    'scripts/ui.js',
+    'scripts/clipboard.js',
+    'scripts/theme.js',
+    'sounds/blop.mp3',
+    'images/favicon-96x96.png'
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+            console.log('[Service Worker] Caching files');
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
-
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+    event.respondWith(
+        caches.match(event.request).then(function(res) {
+            return res || fetch(event.request).then(function(response) {
+                return caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
 });
 
-
 self.addEventListener('activate', function(event) {
-  console.log('Updating Service Worker...')
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-          return true
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
+    event.waitUntil(caches.keys().then(function(cacheNames) {
+        return Promise.all(cacheNames.map(function(key) {
+            if(CACHE_NAME.indexOf(key) === -1) {
+                return caches.delete(key);
+            }
+        }));
+    }));
 });
